083fcccc86a9e93831f40661b33fea980004b44d
Ferdinand <ferdinandvwalree@gmail.com>
2016-01-09 23:47:17 +0100

    Small modification to the gitignore. Added Client file. Created NodeInitialization module, which allows you to startup a NameNode using initNameNode, and any other node using initNode.

---
+++ src/NodeInitialization.hs
@@ -0,0 +1,58 @@
+module NodeInitialization
+(
+initNameNode,
+initNode
+)
+where
+
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+import Network.Transport
+import Network.Transport.TCP
+import System.Environment
+import Data.ByteString.Char8 (pack)
+
+type Host = String
+type Port = String
+type Addr = String
+
+--Use this function to startup the NameNode
+initNameNode :: Process () -> IO ()
+initNameNode p = do
+    args <- getArgs
+    case args of
+        [port] ->      setupNameNode p "127.0.0.1" port
+        [host,port] -> setupNameNode p host port
+
+--Use this function to startup any other node (DataNode/Client)
+initNode :: (ProcessId -> Process ()) -> IO ()
+initNode p = do
+    args <- getArgs
+    --address should be of the form: host:port:0
+    case args of
+        [port,addr] ->      setupNode p "127.0.0.1" port addr
+        [host,port,addr] -> setupNode p host port addr
+            
+        
+setupNode :: (ProcessId -> Process()) -> Host -> Port -> Addr -> IO ()
+setupNode p host port addr = do
+    Right t <- createTransport host port defaultTCPParameters --setup transport layer for the node
+    node <- newLocalNode t initRemoteTable --create a new localnode using the transport layer
+    let nnAddr = EndPointAddress $ pack addr
+        nodeid = NodeId nnAddr --Create a NodeId for the NameNode
+    runProcess node $ do --We start a process on the node
+    whereisRemoteAsync nodeid "NameNodePid" --See if we can find the NameNode and if so get his ProcessId
+    WhereIsReply _ mpid <- expect :: Process WhereIsReply
+    case mpid of
+        Nothing -> liftIO $ putStrLn $ "Could not connect to NameNode with address " ++ addr
+        Just pid -> p pid --Continue with the given Process and pass it the NameNode ProcessId
+        
+setupNameNode :: Process () -> Host -> Port -> IO ()
+setupNameNode p host port = do
+    Right t <- createTransport host port defaultTCPParameters
+    node <- newLocalNode t initRemoteTable
+    runProcess node $ do
+        pid <- getSelfPid --Dynamically register the NameNode's ProcessId
+        register "NameNodePid" pid
+        p
+            
