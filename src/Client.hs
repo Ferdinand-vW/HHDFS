69615bc69abbaac339098a268e3c897cb0d39d3f
Ferdinand <ferdinandvwalree@gmail.com>
2016-01-11 15:52:13 +0100

    Changed FileName to FilePath. Moved some code in ClientAPI to Client

--- src/Client.hs
+++ src/Client.hs
@@ -6,6 +6,7 @@ where
 
 import Control.Distributed.Process
 import Control.Concurrent
+import System.FilePath (takeFileName)
 import qualified Data.ByteString.Lazy.Char8 as B
 import ClientAPI (listFilesReq,writeFileReq,readFileReq)
 import Messages
@@ -16,9 +17,28 @@ client pid = do
   input <- liftIO $ getLine --parse some input
   case words input of
     ["show"] -> do
-      listFilesReq pid --get the filenames from the namenode
+      fsimage <- listFilesReq pid --get the filenames from the namenode
+      showFSImage fsimage
       client pid
     ["write",path] -> writeFileReq pid path >> client pid --Write a file onto the network
-    ["read",name] -> readFileReq pid (B.pack name) >> client pid --Read a file from the network
+    ["read",path] -> do
+        mfdata <- readFileReq pid path --Retrieve the file
+        writeToDisk path mfdata --Write file to disk
+        client pid --Read a file from the network
     ["quit"] -> liftIO $ putStrLn "Closing program..." >> threadDelay 2000000 --Print a message and after 2 seconds quit
     _ -> liftIO (putStrLn "Input was not a valid command.") >> client pid
+    
+
+--Simply prints out all the filenames prefixed with 2 spaces
+showFSImage :: [FilePath] -> Process ()
+showFSImage fsimage = do
+  mapM_ (\x -> liftIO . putStrLn $ "  " ++ x) fsimage
+  
+writeToDisk :: FilePath -> Maybe FileData -> Process ()
+writeToDisk fpath mfdata = do
+    case mfdata of
+        Nothing -> liftIO $ putStrLn "Could not find file on network"
+        Just fdata -> do
+            let fname = takeFileName fpath
+            liftIO $ B.writeFile ("./data/" ++ fname) fdata
+    
--- src/ClientAPI.hs
+++ src/ClientAPI.hs
@@ -7,40 +7,36 @@ readFileReq
 
 import Control.Distributed.Process
 import qualified Data.ByteString.Lazy.Char8 as B
-import System.FilePath (takeFileName)
 import Messages
 
-
---Simply prints out all the filenames prefixed with 2 spaces
-showFSImage :: [FileName] -> Process ()
-showFSImage fsimage = do
-  mapM_ (\x -> liftIO . putStrLn $ "  " ++ (B.unpack x)) fsimage
-
-listFilesReq :: ProcessId -> Process ()
+listFilesReq :: ProcessId -> Process [FilePath]
 listFilesReq pid = do
   (sendPort,receivePort) <- newChan
   send pid (ListFiles sendPort) --Ask the Namenode for the fsimage
-  filelist <- receiveChan receivePort --Wait to receive the fsimage
-  showFSImage filelist
+  receiveChan receivePort --Wait to receive the fsimage
 
 writeFileReq :: ProcessId -> FilePath -> Process ()
 writeFileReq pid fpath = do
   fdata <- liftIO $ B.readFile fpath --Read file into memory (lazily)
-  let fname = B.pack $ takeFileName fpath --get the filename
 
   (sendPort,receivePort) <- newChan
-  send pid (Write fname sendPort) --Send a write request to the namenode
+  send pid (Write fpath sendPort) --Send a write request to the namenode
 
-  (bid,sp) <- receiveChan receivePort --We receive the blockid that has been created for that file
+  (pid,bid) <- receiveChan receivePort --We receive the blockid that has been created for that file
                                       --and we also receive the SendPort of the DataNode
-  sendChan sp (CDNWrite bid fdata) --Send the filedata to the DataNode
+  send pid (CDNWrite bid fdata) --Send the filedata to the DataNode
 
-readFileReq :: ProcessId -> FileName -> Process ()
-readFileReq pid fname = do
+readFileReq :: ProcessId -> FilePath -> Process (Maybe FileData)
+readFileReq pid fpath = do
   (sendPort, receivePort) <- newChan
-  send pid (Read fname sendPort) --Send a Read request to the namenode
+  send pid (Read fpath sendPort) --Send a Read request to the namenode
 
-  mfile <- receiveChan receivePort --Receive the file from the network
-  case mfile of
-    Nothing -> liftIO $ putStrLn "Could not find file on the servers."
-    Just file -> liftIO $ B.writeFile ("./data/" ++ (B.unpack fname)) file
+  mexists <- receiveChan receivePort --Receive the file location
+  
+  case mexists of
+    Nothing -> return Nothing
+    Just (pid,bid) -> do
+        (sp,rp) <- newChan
+        send pid (CDNRead bid sp) --Send a read request to the datanode
+        fdata <- receiveChan rp --Receive the file
+        return $ Just fdata
