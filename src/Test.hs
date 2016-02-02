295870af9a38754a7a06c62a159c83e54864e357
Wilco Kusee <wilcokusee@gmail.com>
2016-02-02 11:26:54 +0100

    Simple test of some large files

--- src/Test.hs
+++ src/Test.hs
@@ -2,36 +2,45 @@ module Test where
 
 import System.Directory
 import Control.Distributed.Process
-import Control.Monad (when)
 import System.Random (mkStdGen, randoms)
+import Control.Monad (zipWithM_)
+import System.FilePath (takeFileName)
+import qualified Data.ByteString.Char8 as B
 
---client :: ProcessId -> Process ()
---client pid = do
---  input <- liftIO getLine --parse some input
---  case words input of
---    ["show"] -> do
---      fsimage <- listFilesReq pid --get the filenames from the namenode
---      showFSImage fsimage
---      client pid
---    ["write",localFile,remotePath] -> writeFileReq pid localFile remotePath >> client pid --Write a file onto the network
---    ["read",path] -> do
---        mfdata <- readFileReq pid path --Retrieve the file
---        writeToDisk path mfdata --Write file to disk
---        client pid --Read a file from the network
---    ["quit"] -> liftIO $ putStrLn "Closing program..." >> threadDelay 2000000 --Print a message and after 2 seconds quit
---    ["shutdown"] -> liftIO (putStrLn "Shutting down Network...") >> shutdownReq pid >> terminate
---    _ -> liftIO (putStrLn "Input was not a valid command.") >> client pid
+import Messages (FileData)
+import ClientAPI (listFilesReq,writeFileReq,readFileReq, shutdownReq)
 
 testDir :: String
 testDir = "./test_files"
 
+testFileCount :: Int
+testFileCount = 3
+
 testClient :: ProcessId -> Process ()
 testClient pid = do
   liftIO $ createTestDir
-  let ints = randoms (mkStdGen 0) :: [Int]
-  let (file, ints') = generateFile ints
+  let files = zip generateFiles [0..]
+
+      getFilename n = testDir ++ "/file" ++ show n
+      fileIn n  = getFilename n ++ ".in"
+      fileOut n = getFilename n ++ ".out"
+
+  liftIO $ putStrLn "Generating files..."
+  liftIO $ mapM_ (\(f,n) -> writeFile (fileIn n) f) (take testFileCount files)
+
   liftIO $ putStrLn "Starting tests..."
-  liftIO $ writeFile (testDir ++ "/file1.txt") file
+  let fileNums = [0 .. testFileCount - 1]
+      testFile n = do
+        liftIO $ putStrLn $ "write " ++ (fileIn n) ++ " " ++ (fileOut n)
+        writeFileReq pid (fileIn n) (fileOut n)
+  mapM_ testFile fileNums
+
+  let readBack n = do
+        let filename = "file" ++ show n ++ ".out"
+        liftIO $ putStrLn $ "read " ++ fileOut n
+        file <- readFileReq pid (fileOut n)
+        writeToDisk filename file
+  mapM_ readBack fileNums
 
 createTestDir :: IO ()
 createTestDir = do
@@ -41,13 +50,27 @@ createTestDir = do
     False -> return ()
   createDirectory testDir
 
+generateFiles :: [String]
+generateFiles = go ints
+  where ints = randoms (mkStdGen 0) :: [Int]
+        go as = let (file, ys) = generateFile as
+                in  file : go ys
+
 generateFile :: Show a => [a] -> (String, [a])
-generateFile as = go 100000 as ""
+generateFile as = go (100000 :: Int) as ""
   where go 0 as acc = (acc, as)
-        go n as acc = go (n-1) as' (line ++ acc)
+        go n as acc = go (n-1) as' (line ++ "\n"++ acc)
           where (line, as') = generateLine as
 
 generateLine :: Show a => [a] -> (String, [a])
 generateLine as = (text, ys)
   where (xs,ys) = splitAt 10 as
         text = concatMap ((' ':) . show) xs
+
+
+writeToDisk :: FilePath -> Maybe FileData -> Process ()
+writeToDisk fpath mfdata = case mfdata of
+  Nothing -> liftIO $ putStrLn "Could not find file on network"
+  Just fdata -> liftIO $ do
+      createDirectoryIfMissing False "./local"
+      B.writeFile ("./local/" ++ takeFileName fpath) fdata
